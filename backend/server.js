3a04c7a6c3f43bc0e0146c0daec80a4fa9f867ba
aishwarya25252 <aishwarya25252@gmail.com>
2019-12-27 10:22:14 +0530

    *

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-//to be added
+const express = require('express')
+const serve = express()
+const port = 3000
+serve.get('/',(req,res)=>res.send(`started on port : ${port} !`))
+serve.listen(port,() => console.log(`server started on port : ${port} !`))
+

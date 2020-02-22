17bd37eb223337b6578d9243cee8c6b3c57e8f38
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-02-22 21:32:23 +0530

    added learning part

--- geoid/mern-geoid/backend_piyush/server.js
+++ geoid/mern-geoid/backend_piyush/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 
 
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,14 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.GEOID_URI;
-mongoose.connect(uri, { useUnifiedTopology: 
-    true, useNewUrlParser: true, useCreateIndex: true }
-    );
-    const connection = mongoose.connection;
-    connection.once('open', () => {
-        console.log("MongoDB database connection established successfully");
-    })
     
 const usersRouter = require('./routes/users');
 
--- geoid/mern-geoid/src/index.js
+++ geoid/mern-geoid/src/index.js
@@ -4,4 +4,14 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDmb-XT3bSaeviYxRZIgjsFw6BLcKRfXBw",
+    authDomain: "geoid-e9ac7.firebaseapp.com",
+    databaseURL: "https://geoid-e9ac7.firebaseio.com",
+    projectId: "geoid-e9ac7",
+    storageBucket: "geoid-e9ac7.appspot.com",
+    messagingSenderId: "1003784498111",
+    appId: "1:1003784498111:web:694e80f1ff8dd65df6cf0a",
+    measurementId: "G-4C0MFJND05"
+  };
 
